bcef949d1af8df0df56f4a9927973949e4f4604d
Marosey09 <145623689+Marosey09@users.noreply.github.com>
2024-02-29 20:15:54 -0500

    add name authentication

--- Community-Code.cpp
+++ Community-Code.cpp
@@ -19,6 +19,19 @@ if (name == "Joe")
     cout << "I did it JOOOOOOOOOOOEEEEEEEEEEE" << endl;
 }
 
+if (name == "Gavyn")
+{
+    cout << "You should make sure Joe isn't being illegal" << endl;
+
+}
+
+if (name == "Matthew")
+{
+    cout << "Also check up on Joe for his illegal activities" << endl;
+}
+
+
+
 return 0;
 
 }
