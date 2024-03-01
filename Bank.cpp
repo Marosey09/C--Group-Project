3ed8b91613f0c5f88e2363d234126a5465ffaa9a
Marosey09 <145623689+Marosey09@users.noreply.github.com>
2024-02-29 20:44:01 -0500

    $$$

---
+++ Bank.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ //give ideas in dsicord before working on this   
+
+cout << "Hello World" << endl;
+
+string name;
+
+string pin;
+
+string deposit;
+
+string withdraw;
+
+cout << "Please enter name" << endl;
+
+cin >> name;
+
+
+
+
+
+return 0;
+
+}
